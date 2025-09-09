5203ad647c94e5cc05e715dc58aae50301af474f
Ashish Mishra <asismisr@CTIPL-MAC-348.local>
2025-09-10 02:10:15 +0530

    updting the guthib yml file

--- scripts/parse_test_results.py
+++ scripts/parse_test_results.py
@@ -1,13 +1,17 @@
-# scripts/parse_test_results.py
 import xml.etree.ElementTree as ET
 
 tree = ET.parse("report.xml")
 root = tree.getroot()
 
-total = int(root.attrib.get("tests", 0))
-failures = int(root.attrib.get("failures", 0))
-errors = int(root.attrib.get("errors", 0))
-skipped = int(root.attrib.get("skipped", 0))
+total = failures = errors = skipped = 0
+
+# Sum attributes from all <testsuite> elements
+for ts in root.findall("testsuite"):
+    total += int(ts.attrib.get("tests", 0))
+    failures += int(ts.attrib.get("failures", 0))
+    errors += int(ts.attrib.get("errors", 0))
+    skipped += int(ts.attrib.get("skipped", 0))
+
 passed = total - failures - errors - skipped
 
 summary = (
